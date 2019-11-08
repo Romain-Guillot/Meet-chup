3502abb81294a96861f08dcf7a25a582e61eb3af
Romain Guillot <rguillot42@gmail.com>
2019-11-07 21:59:00 -0500

    fix unused import

--- app/src/main/java/com/example/appprojet/repositories/FirebaseAuthenticationRepository.java
+++ app/src/main/java/com/example/appprojet/repositories/FirebaseAuthenticationRepository.java
@@ -1,7 +1,5 @@
 package com.example.appprojet.repositories;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -16,7 +14,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
