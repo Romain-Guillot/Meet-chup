dd09da7c92695a5a395f820c029d03aca3e2e31c
Alex <alexis@lattard.fr>
2019-12-06 17:57:28 -0500

    Photos visible in album

--- app/src/main/java/com/progmobile/meetchup/ui/event_view/AlbumFragment.java
+++ app/src/main/java/com/progmobile/meetchup/ui/event_view/AlbumFragment.java
@@ -1,24 +1,45 @@
 package com.progmobile.meetchup.ui.event_view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.progmobile.meetchup.R;
+import com.progmobile.meetchup.models.Post;
+import com.progmobile.meetchup.ui.event_view.adapters.PhotosListViewAdapter;
+import com.progmobile.meetchup.ui.post_creation.PostCreationActivity;
+import com.progmobile.meetchup.ui.post_view.PostViewActivity;
+import com.progmobile.meetchup.utils.views.EventMetaData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TBD
  */
 public class AlbumFragment extends Fragment {
 
+    private EventViewViewModel viewModel;
+
+    private EventMetaData eventMetaData;
+    private RecyclerView photosView;
+    private ViewGroup emptyPhotosContainer;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(getActivity()).get(EventViewViewModel.class);
     }
 
 
@@ -26,6 +47,66 @@ public class AlbumFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_event_view_album, container, false);
+
+        eventMetaData = view.findViewById(R.id.event_album_metadata);
+
+        photosView = view.findViewById(R.id.event_album);
+        photosView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        emptyPhotosContainer = view.findViewById(R.id.event_empty_photo);
+
+        FloatingActionButton addPostFAB = view.findViewById(R.id.album_add_post_fab);
+        addPostFAB.setOnClickListener(v ->
+                launchCreationPostActivity()
+        );
+
+        Button addPostButton = view.findViewById(R.id.event_empty_photo_add_btn);
+        addPostButton.setOnClickListener(v ->
+                launchCreationPostActivity()
+        );
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        viewModel.eventMetaData.observe(this, event -> {
+            if (event != null)
+                eventMetaData.setMetaData(event);
+        });
+
+        viewModel.eventPosts.observe(this, posts -> {
+            posts = PreprocessListPosts(posts);
+            if (posts.isEmpty()) {
+                emptyPhotosContainer.setVisibility(View.VISIBLE);
+                photosView.setVisibility(View.GONE);
+            } else {
+                emptyPhotosContainer.setVisibility(View.GONE);
+                photosView.setVisibility(View.VISIBLE);
+                photosView.setAdapter(new PhotosListViewAdapter(posts, post -> {
+                    Intent intent = new Intent(getActivity(), PostViewActivity.class);
+                    intent.putExtra(PostViewActivity.EXTRA_POST_ID, post.getId());
+                    intent.putExtra(PostViewActivity.EXTRA_EVENT_ID, viewModel.eventID);
+                    startActivity(intent);
+                }));
+            }
+        });
+    }
+
+    private List<Post> PreprocessListPosts(List<Post> posts){
+        List<Post> l = new ArrayList<>();
+        for (Post p : posts){
+            if(p.getDocUrl() != null){
+                l.add(p);
+            }
+        }
+        return l;
+    }
+
+
+    private void launchCreationPostActivity() {
+        Intent intent = new Intent(getActivity(), PostCreationActivity.class);
+        // put extras
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/progmobile/meetchup/ui/event_view/adapters/PhotosListViewAdapter.java
@@ -0,0 +1,111 @@
+package com.progmobile.meetchup.ui.event_view.adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.progmobile.meetchup.R;
+import com.progmobile.meetchup.models.Post;
+import com.progmobile.meetchup.repositories.FirebaseStorageRepository;
+import com.progmobile.meetchup.repositories.IStorageRepository;
+import com.progmobile.meetchup.utils.Callback;
+import com.progmobile.meetchup.utils.CallbackException;
+
+import java.util.List;
+
+public class PhotosListViewAdapter extends RecyclerView.Adapter<PhotosListViewAdapter.PhotoViewHolder> {
+
+    private static IStorageRepository storageRepository;
+    private List<Post> posts;
+    private PhotosListViewAdapter.OnItemClickListener listener;
+
+    public PhotosListViewAdapter(List<Post> posts, OnItemClickListener clickListener) {
+        this.posts = posts;
+        this.listener = clickListener;
+        storageRepository = FirebaseStorageRepository.getInstance();
+    }
+
+    public static class PhotoViewHolder extends RecyclerView.ViewHolder {
+        View view;
+        ImageView imageView;
+        ProgressBar loadingView;
+        Context context;
+
+        public PhotoViewHolder(View itemView) {
+            super(itemView);
+            this.view = itemView;
+            this.imageView = itemView.findViewById(R.id.item_photo_image);
+            this.loadingView = itemView.findViewById(R.id.item_photo_loading);
+            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+itemView);
+            context = itemView.getContext();
+        }
+
+        public void bind(Post post, final PhotosListViewAdapter.OnItemClickListener listener) {
+            if (listener != null)
+                view.setOnClickListener(v -> listener.onItemClick(post));
+
+            if (post == null) {
+                loadingView.setVisibility(View.GONE);
+                imageView.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources().getDisplayMetrics());
+                imageView.setImageDrawable(context.getDrawable(R.drawable.ic_error_image));
+                return ;
+            }
+
+            String docURL = post.getDocUrl();
+            if (docURL != null) {
+                loadingView.setVisibility(View.VISIBLE);
+                PhotosListViewAdapter.storageRepository.getData(docURL, new Callback<byte[]>() {
+                    public void onSucceed(byte[] result) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(result, 0, result.length);
+                        imageView.setImageBitmap(bitmap);
+                        loadingView.setVisibility(View.GONE);
+                        imageView.setVisibility(View.VISIBLE);
+                    }
+                    public void onFail(CallbackException exception) {
+                        loadingView.setVisibility(View.GONE);
+                        imageView.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources().getDisplayMetrics());
+                        imageView.setImageDrawable(context.getDrawable(R.drawable.ic_error_image));
+                    }
+                });
+            } else {
+                imageView.setVisibility(View.GONE);
+                loadingView.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View postView = inflater.inflate(R.layout.item_photo, parent, false);
+        PhotoViewHolder holder = new PhotoViewHolder(postView);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post, listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+
+    public interface OnItemClickListener{
+        void onItemClick(Post post);
+    }
+}
